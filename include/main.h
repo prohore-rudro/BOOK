5040fcd3ddcca03352ba789286292b4b8b60df49
Prohore Rudro <raiantasfin@yandex.com>
2020-04-18 14:38:59 +0600

    Book Class
    Book Class declaration

--- include/main.h
+++ include/main.h
@@ -1,39 +1,44 @@
-/*  INCLUDE IN MAIN */
+/* Main Header */
 
-#ifndef FPATH_H
-#define FPATH_H
+#ifndef MAIN_H
+#define MAIN_H
 
-/*  IO  */
+/*  Dependenet HEADERS*/
+/*  IO */
 #include <iostream>
 
-/*  STL  */
-#include <string>
-#include <vector>i
+/*  STL */
 #include <array>
+#include <vector>
+#include <map>
+#include <string>
 
-/*  Global namespace for project */
-namespace fpath{
-    /*  Depended Classes*/
-    /*  These are defined in separate header files*/
-    class Directory;
+/*  GLOBAL NAMESPACE FOR ENTIRE PROJECT */
+namespace book{
+    /* CONSTANT declarations*/    
+    const uint8_t AVAIL_ERR = 2; // number of defined errors
 
-    /*  Constant Declarations */
-    const std::size_t AVAIL_ERR=2; // number of available errors
-
-    /*  Type Declarations */
-    using file_t = std::string; // name of files
+    /* Type Declarations */
     using name_t = std::string; // name of any kind
+    using file_t = std::string  // name of file
 
-    using serial_t = int32_t;   // id of elements
-    using error_t = int32_t;    // error number
-    using cnt_t = int32_t;      // count of files
-    
-    using error_list_t = std::array<std::string, AVAIL_ERR>; // list of error messages
+    using serial_t = int32_t;   // number id for strings
+    using error_t = int32_t;    // error code
+    using cnt_t = int32_t;      // counting number
 
-    /*  ERROR LIST */
+    using error_list_t = std::array<name_t,AVAIL_ERR>; // list of defined errorss
+
+    /* Error Codes */
     error_list_t error_list;
-    error_list[0] = "OPERATION SUCCESSFULL";
-    error_list[1] = "FAILED EXECUTION";
+    
+    error_list[0] = "Execution Succesful";
+    error_list[1] = "Execution Failed";
+
+    /* CLASS Declarations */
+    /* Main class */
+    class Dbase;    // main class
+    class Book;     // class to store books
+    class Directory; // directories are treated as tags
 }
 
-#endif // FPATH
+#endif // MAIN_H
