34d88e31ee5676adbfab89418a219224024ab8d9
prohore-rudro <48759760+prohore-rudro@users.noreply.github.com>
2020-04-18 22:53:46 +0600

    Invalid Serial Declaration
    -1 for invalid serial

--- include/main.h
+++ include/main.h
@@ -15,18 +15,13 @@
 
 /*  GLOBAL NAMESPACE FOR ENTIRE PROJECT */
 namespace book{
-    /* CONSTANT declarations*/    
-    const uint8_t AVAIL_ERR = 2; // number of defined errors
-
-    /* GLOBAL variable declaration */
-    int ErrorMessage(0);
 
     /* Type Declarations */
     using name_t = std::string; // name of any kind
     using file_t = std::string; // name of file
     using path_t = std::string; // path type
 
-    using serial_t = int32_t;   // number id for strings
+    using serial_t = int64_t;   // number id for strings
     using error_t = uint8_t;    // error code
     using option_t = uint8_t;   // option code
     using cnt_t = int32_t;      // counting number
@@ -34,6 +29,10 @@ namespace book{
     using error_list_t = std::array<name_t,AVAIL_ERR>; // list of defined errorss
     using name_list_t = std::vector<name_t>; // list of names
 
+    /* CONSTANT declarations*/    
+    const uint8_t AVAIL_ERR = 2; // number of defined errors
+    const serial_t INVALID_SERIAL = -1;
+    
     /* Error Codes */
     error_list_t error_list;
     
